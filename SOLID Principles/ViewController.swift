e49a22e0a8c364bb644261124179cd0b3feef825
Raja Musharraf <Musharraf@Raja-Macbook.local>
2026-02-18 10:00:06 +0500

    inital commit

---
+++ SOLID Principles/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOLID Principles
+//
+//  Created by Raja Musharraf on 18/02/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
